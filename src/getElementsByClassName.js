5bbedd888d96ba32840a8bfa1f0ee71ab6404f00
mariomui <yi.kan.mui@gmail.com>
2018-10-12 13:16:53 -0500

    getElementByClassName finished
    Document.body may be an object but it has too much stuff to put into the
    recursion. It's got other things chocked in it. The pseudocode was if this
    is a collection...But the code was, does this have childnodes.
    If this has child nodes, bring it up with the childNodes method because
    body is an object.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,27 +4,36 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function (className) {
-    // your code here
+var getElementsByClassName = function(className) {
     //last part of the tree is when there are no more elements in body.
     //body.childNodes[i] = body.childNodes[3] (lets say.)
     //
     var answerArr = [];
-    var body = document.body;
+    var bodyNodes = document.body;
+    console.log(bodyNodes, bodyNodes.childElementCount);
+    //looping 4 times but i have at least 5 elements.
+    var recurseThis = function (bodyNodes) {
+        //this recursion is stopping early.
+        if ( bodyNodes.classList && bodyNodes.classList.contains(className) ) {
+            console.log("hello");
+            //this part isnt registering at all.
+            // console.log(bodyNodes);
+            answerArr.push(bodyNodes);
+        }         
+        if ( bodyNodes.hasChildNodes() ) {
+            //this should do it first.
+            //the block inside here does run.
+            //nope that doesn't maek sense either.
+            // _.each(bodyNodes.childNodes, item => {
+            for (var i = 0; i < bodyNodes.childNodes.length; i++) {
 
-    var recurse = function (body) {
-        if (body.classList && body.classList.contains(className)) {
-            //last part of the tree is element that i find in the element 3.
-            //this time i get an empty array. debug time.
-            console.log()
-            answerArr.push(body);
-        }
-        if (body.hasChildNodes()) {
-            for (var i = 0; i < body.length - 1; ++i) {
-                recurse(body.childNodes[i]);
+                recurseThis(bodyNodes.childNodes[i]);
             }
-        }
+            // })
+            
+        } 
+        
     }
-    recurse(body); //necessary to call it. otherwise it's just a function expression.
+    recurseThis(bodyNodes); //necessary to call it. otherwise it's just a function expression.
     return answerArr;
 };
