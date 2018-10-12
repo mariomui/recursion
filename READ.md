88e0deeac6ffd254447e12e77b771a9da414925e
mariomui <yi.kan.mui@gmail.com>
2018-10-12 10:58:56 -0500

    First Commit
    Some psuedocode and some initial problems.
    How to countdown when the parameter can't be used to do so?

---
+++ READ.md
@@ -0,0 +1,10 @@
+Replace stringifyJSON with your own function in src/stringifyJSON.js, and make the specs pass.
+
+Implement getElementsByClassName with your own function in src/getElementsByClassName.js, and make the specs pass.
+
+You should use 
+document.body, element.childNodes, and element.classList
+
+Get a collection of the <body> element's child nodes:
+
+var c = document.body.childNodes;
--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,26 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
+var getElementsByClassName = function(className) {
   // your code here
+  //base case
+  //This is a permuation problem.
+  //
+  //for all nodes in document.body
+  //if node has childNodes do this recursion.
+  //else push it the up top and next!
+
+    var answerArr = [];
+    const bodyNodes = document.body.childNodes;
+    for (let i = 0; i < bodyNodes.length; ++i) {
+        //intrinsic problem lies in how to count downn when only the classname is involved.
+        if (bodyNodes[i].hasChildNodes()) {
+            answer.concat( getElementsByClassName(className) );
+        } else {
+            if (bodyNodes[i].classList.value.indexOf(className) > -1) {
+                answerArr.push(bodyNodes[i]);
+            }
+        }
+    }
+    return answerArr;
 };
