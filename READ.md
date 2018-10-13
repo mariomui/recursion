b5d2ab8e10d85ddbab15d6594bba58c58d955dd5
mariomui <yi.kan.mui@gmail.com>
2018-10-12 19:52:41 -0500

    Chabudashi 2.1

--- READ.md
+++ READ.md
@@ -1,4 +1,41 @@
-Replace stringifyJSON with your own function in src/stringifyJSON.js, and make the specs pass.
+# TEST SUITE
+
+node testsite.js to run tests
+OR
+npm run tryit
+
+
+# Replace stringifyJSON with your own function in src/stringifyJSON.js, and make the specs pass.
+
+//inputs
+[1, 2, [3, 4]]
+'[1,2,[3,4]]'
+[1,2,3,4]
+{a:1, a:2, a:3}
+'{"a":1,"b":2,"c":3}'
+
+[1,2,{a: 1}]
+'[1,2,{"a":1}]'
+
++ check if array or object
++ Order of results:
+```js
+ ']', '}', '1', ':', '"', 'a', '"', '{', ',', '2', ',', '1', '['
+
+//f [1,2]
+//b [2,1]
+//forwards   [1,2,{"a":1}]
+ //backwards  ]}1:"a"{,2,1[
+
+// this is the same as the recursively flatten an array problem , only with the added wringle of adding the brackets a strings. so base case would be if x.length doesn't exist, add the []
+//if item isnt' an object don't recurse.
+```
+//split up the behaviros, decision tree
+//is array, object or value?
+//array call recursion and add to answer, object do this call recursion and parse before adding to answer, value add to answer.
+
+//hand it off 
+
 
 Implement getElementsByClassName with your own function in src/getElementsByClassName.js, and make the specs pass.
 
---
+++ test.js
@@ -0,0 +1,49 @@
+module.exports = {
+    each: (collection, callback) => {
+        if (Array.isArray(collection)) {
+            for (var i = 0; i < collection.length; i++) {
+                callback(collection[i], i, collection);
+            }
+        } else {
+            for (var key in collection) {
+                callback(collection[key], key, collection);
+            }
+        }
+    },
+
+    parse: (item, key) => {
+        return `"${key}":${item}`;
+
+    },
+
+    assertEquals: function (actual, expected, testName) {
+        var act = JSON.stringify(actual);
+        var exp = JSON.stringify(expected);
+        if (act === exp) {
+            console.log('passed:' + testName);
+        } else {
+            console.log(`[${testName}] failed: expected ${expected} but got ${actual}`);
+        }
+    },
+
+    isWhat: function (target) {
+        if (typeof target === 'object' && !(target == null)) {
+            if (Array.isArray(target)) {
+                return 'array';
+            }
+            return 'object';
+        } 
+        if (typeof target === 'number') {
+            return 'number';
+        }
+        if (typeof target === 'string') {
+            return 'string';
+        }
+        if (typeof target == 'null') {
+            return 'null';
+        }
+        if (typeof target == 'boolean') {
+            return 'boolean';
+        }
+    }
+}
