e7a532b94cd0050e743c5e47e2335a0b1fd63437
mariomui <yi.kan.mui@gmail.com>
2018-10-12 19:27:32 -0500

    Can't Use Helper Recursive Function
    So Chabudashi.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -8,54 +8,64 @@ var stringifyJSON = function(obj) {
   // your code goes here
   var answer = '';
   var flag = false;
+
   var recurseThis = function(obj) {
     //if item isn't an object, that means it's a value, so i want that straight in.
-    if (!isWhat(obj)) {
+
+    if (typeof obj === 'number') {
       answer += obj;
-      if (flag) {
-        answer += ']';
-        flag = false;
-      } 
-        answer +=' ';
+      answer += ' ';
+
+      // if (flag === true) {
+      //   // answer += obj;
+      //   answer += ']';
+      //   flag = false;
+      // }
+   
     
     }
+   
     // this is the wrong basecase.
     // console.log(Object.is(obj));
     //if is an object. 
-    if (typeof obj === 'object' && !Array.isArray(obj)) {
+    if (isWhat(obj) === 'object') {
       each(obj, (item, key) => {
         answer += `"${key}":`;
         recurseThis(item);
       });
-    } else if (Array.isArray(obj)) {
+    } 
+    if (Array.isArray(obj)) {
       each( obj, (item, key) => {
+
         // answer += (`${item}`);
         if (key === 0) {
           answer += '[';
         }
-        if (key === obj.length-1) {
-          flag = true;
-        }
         recurseThis(item);
+
+        if (key === obj.length-1) {
+          answer = answer + '] ';
+          answer = answer.trim();
+
+        } 
+
       })
     }
   }
-  
   recurseThis(obj);
+  // answer = answer.slice(0,-1);
   // if (isWhat(obj) === 'array') {
-  //   answer = '['+answer+']';
+  //   answer = answer+']';
   // }
-  answer = answer.trim();
   
-  // if (isWhat(obj) === 'array') {
-  //   answer += ']';
-  // }
   if (isWhat(obj) === 'object') {
     answer = '{'+ answer;
-    answer += '}';
+    answer = answer+ '}';
   }
+  var dart = answer.lastIndexOf(' ');
+  answer = answer.substring(0,dart)+answer.substring(dart+1,answer.length);
+  console.log(answer+'!');
   return answer.replace(/ /g,',');
-  return answer;
 };
 
 module.exports = stringifyJSON;
